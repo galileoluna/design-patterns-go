52d946dbe7a7d45022f842246852e5ac20adc2c9
Galileo Luna <galileoungs@gmail.com>
2020-12-02 20:47:49 -0300

    genero comentarios :)

--- singleton/main.go
+++ singleton/main.go
@@ -4,9 +4,10 @@ package singleton
 type singleton struct {
 	contador int
 }
-
+//definimos un puntero a una estructura de tipo Singleton como nil, 
+//y lo asignamos a la variable  Instancia.
 var Instancia *singleton
-
+//Verificamos que si la instancia no se inicializa se crea una instancia en el espacio ya asignado
 func GetInstancia() *singleton {
 	if Instancia == nil {
 		Instancia = new(singleton)
@@ -14,7 +15,7 @@ func GetInstancia() *singleton {
 
 	return Instancia
 }
-
+//Le agregamos 1 al valor del contador y nos retorna el valor del contador
 func (s *singleton) AgregarUno() int {
 	s.contador++
 	return s.contador
