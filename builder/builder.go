0d5e7c9cdf20abd02bcbc87c6e156cac48e05c59
Galileo Luna <galileoungs@gmail.com>
2020-12-09 21:12:45 -0300

    termino el patron builder

---
+++ builder/builder.go
@@ -0,0 +1,100 @@
+package builder
+
+type BuildProcess interface {
+	SetRuedas() BuildProcess
+	SetAsientos() BuildProcess
+	SetEstructura() BuildProcess
+	GetVehiculo() VehiculoProduto
+}
+
+//Director
+type ManufacturingDirector struct {
+	builder BuildProcess
+}
+
+func (f *ManufacturingDirector) Construct() {
+	f.builder.SetAsientos().SetEstructura().SetRuedas()
+}
+
+func (f *ManufacturingDirector) SetBuilder(b BuildProcess) {
+	f.builder = b
+}
+
+// Estructura del VehiculoProducto
+type VehiculoProduto struct {
+	Ruedas     int
+	Asientos   int
+	Estructura string
+}
+
+//Constructor de tipo Auto
+type AutoBuilder struct {
+	v VehiculoProduto
+}
+
+func (c *AutoBuilder) SetRuedas() BuildProcess {
+	c.v.Ruedas = 4
+	return c
+}
+
+func (c *AutoBuilder) SetAsientos() BuildProcess {
+	c.v.Asientos = 5
+	return c
+}
+
+func (c *AutoBuilder) SetEstructura() BuildProcess {
+	c.v.Estructura = "Auto"
+	return c
+}
+
+func (c *AutoBuilder) GetVehiculo() VehiculoProduto {
+	return c.v
+}
+
+//Constructor de tipo Moto
+type MotoBuilder struct {
+	v VehiculoProduto
+}
+
+func (b *MotoBuilder) SetRuedas() BuildProcess {
+	b.v.Ruedas = 2
+	return b
+}
+
+func (b *MotoBuilder) SetAsientos() BuildProcess {
+	b.v.Asientos = 2
+	return b
+}
+
+func (b *MotoBuilder) SetEstructura() BuildProcess {
+	b.v.Estructura = "Motocicleta"
+	return b
+}
+
+func (b *MotoBuilder) GetVehiculo() VehiculoProduto {
+	return b.v
+}
+
+//Constructor de tipo Colectivo
+type ColectivoBuilder struct {
+	v VehiculoProduto
+}
+
+func (b *ColectivoBuilder) SetRuedas() BuildProcess {
+	b.v.Ruedas = 8
+	return b
+}
+
+func (b *ColectivoBuilder) SetAsientos() BuildProcess {
+	b.v.Asientos = 30
+	return b
+}
+
+func (b *ColectivoBuilder) SetEstructura() BuildProcess {
+	b.v.Estructura = "Colectivo"
+	return b
+}
+
+func (b *ColectivoBuilder) GetVehiculo() VehiculoProduto {
+	return b.v
+}
---
+++ builder/builder_test.go
@@ -0,0 +1,62 @@
+package builder
+
+import (
+	"testing"
+)
+
+func TestBuilderPattern(t *testing.T) {
+	manufacturingComplex := ManufacturingDirector{}
+
+	autoBuilder := &AutoBuilder{}
+	manufacturingComplex.SetBuilder(autoBuilder)
+	manufacturingComplex.Construct()
+
+	auto := autoBuilder.GetVehiculo()
+
+	if auto.Ruedas != 4 {
+		t.Errorf("Ruedas deben ser 4 pero son %d\n", auto.Ruedas)
+	}
+
+	if auto.Estructura != "Auto" {
+		t.Errorf("Estructura debe ser  'auto' pero es %s\n", auto.Estructura)
+	}
+
+	if auto.Asientos != 5 {
+		t.Errorf("Un auto debe tener  5 pero tiene %d\n", auto.Asientos)
+	}
+
+	motoBuilder := &MotoBuilder{}
+
+	manufacturingComplex.SetBuilder(motoBuilder)
+	manufacturingComplex.Construct()
+
+	moto := motoBuilder.GetVehiculo()
+	moto.Asientos = 1
+
+	if moto.Ruedas != 2 {
+		t.Errorf("Ruedas debe  tener 2 ruedas pero tiene %d\n", moto.Ruedas)
+	}
+
+	if moto.Estructura != "Motocicleta" {
+		t.Errorf("Estructura de la moto  debe ser 'Motocicleta' pero es %s\n", moto.Estructura)
+	}
+
+	colectivoBuilder := &ColectivoBuilder{}
+
+	manufacturingComplex.SetBuilder(colectivoBuilder)
+	manufacturingComplex.Construct()
+
+	colectivo := colectivoBuilder.GetVehiculo()
+
+	if colectivo.Ruedas != 8 {
+		t.Errorf("Un colectivo debe tener 8 ruedas ,pero tiene ...  %d\n", colectivo.Ruedas)
+	}
+
+	if colectivo.Estructura != "Colectivo" {
+		t.Errorf("La estructura del colectivo debe ser  'colectivo' pero es... %s\n", colectivo.Estructura)
+	}
+
+	if colectivo.Asientos != 30 {
+		t.Errorf("Un colectivo debe tener 30 asientos pero tiene %d\n", colectivo.Asientos)
+	}
+}
